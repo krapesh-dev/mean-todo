e42583f87a56547858a8e1f59c7f72f5c49631b4
Krapesh <krapesh.dev@gmail.com>
2014-11-22 16:58:20 +0530

    Included routes cdn, changes in core file

--- public/core.js
+++ public/core.js
@@ -1,39 +1,40 @@
-var scotchIO = angular.module('scotchIO', ['ngRoute'] {
-  function mainController($scope, $http) {
-    $scope.formData = {};
+var scotchIO = angular.module('scotchTodo', ['ngRoute']);
 
-    // get all todos
-    $http.get('/api/todos')
+function mainController($scope, $http) {
+  $scope.formData = {};
+
+  // get all todos
+  $http.get('/api/todos')
+    .success(function(data) {
+      $scope.todos = data;
+      console.log(data);
+    })
+    .error(function(data) {
+      console.log('Error: ' + data);
+    });
+
+  // when submitting the add form, send the text to the node API
+  $scope.createTodo = function() {
+    $http.post('/api/todos', $scope.formData)
+      .success(function(data) {
+        $scope.formData = {}; // clear the form so our user is ready to enter another
+        $scope.todos = data;
+        console.log(data);
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
+      });
+  };
+
+  // delete a todo after checking it
+  $scope.deleteTodo = function(id) {
+    $http.delete('/api/todos/' + id)
       .success(function(data) {
         $scope.todos = data;
         console.log(data);
-      });
-      .error(function(data) {
-        console.log('Error ' + data);
       })
-
-    // create todo
-    $scope.createTodo = function() {
-      $http.post('/api/todos', $scope.formData)
-        .success(function(data) {
-          $scope.formData = {};
-          $scope.todos = data;
-          console.log(data);
-        })
-        .error(function(data) {
-          console.log('Error ' + data);
-        })
-
-    // delete a todo
-    $scope.deleteTodo = function() {
-      $http.delete('/api/todos' + id)
-        .success(function(data) {
-          $scope.todos = data;
-          console.log(data);
-        })
-        .error(function(data) {
-          console.log('Error ' + data);
-        })
-    };
-  });
-});
+      .error(function(data) {
+        console.log('Error: ' + data);
+      });
+  };
+}
